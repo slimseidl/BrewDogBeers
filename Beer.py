02ba3fe42fb5d72764593776d527ea542d625869
John Seidl <jseidl2@wgu.edu>
2025-09-26 14:17:27 -0500

    Built Beer Class

---
+++ Beer.py
@@ -0,0 +1,80 @@
+import requests 
+import json
+import pprint
+
+class Beer():
+    def __init__(self):
+        pass 
+
+# Base API = https://punkapi.online/v3/
+# Random beer = beers/random
+# Beer by ID = beers/{id}
+# All beers by page (14 pages, 30 per page) = beers?page={page_number}
+# Image of beer by id = images/{id}.png
+
+# Optional parameters for /beer
+# per_page = specify the number of items to return per page, the value can range from 10 to 80, 30 by default
+# beer_name={string} =  retrieve a list of beers that match the specified string
+# abv_gt={number} = retrieve a list of beers that have an ABV greater than the specified number
+# abv_lt={number} = retrieve a list of beers that have an ABV less than the specified number
+
+    def get_all_beers(self):
+        beer_list = [] # empty list to append dictionaries to
+
+        for page in range(1,30): 
+
+            url = f'https://punkapi.online/v3/beers?page={page}&per_page=30' # API Call 
+            response = requests.get(url).json()
+
+            # JSON response returns a list of dicts corresponding to each beer
+            # for beer (every dictionary) in the json response
+            # create a new dictionary with specified data
+            # append every beer dictionary to a beer list for further processing
+            for beer in response:
+                beer_data = {
+                    "number":beer["id"],
+                    "name": beer["name"],
+                    "tag": beer["tagline"],
+                    "content": beer["abv"],
+                    "ibus": beer["ibu"],
+                    "first_brewed": beer["first_brewed"]
+                }
+                beer_list.append(beer_data)
+
+        return beer_list
+
+    def get_beer_id(self, beerName):
+        all_beer = self.get_all_beers()
+        for beer in all_beer:
+            if beerName.lower() in beer["name"].lower():
+                return beer["number"]
+            
+
+
+    def get_hops_list(self, beerName):
+
+        url = f"https://punkapi.online/v3/beers/{self.get_beer_id(beerName)}"
+        response = requests.get(url).json()
+
+        if isinstance(response, list):
+            beer = response[0]
+        else:
+            beer = response
+
+        hops = beer["ingredients"]["hops"]
+
+        hop_dict = {}
+        for val in hops:
+            hop_type = val["name"]
+            attribute = val["attribute"]
+
+            if hop_type not in hop_dict:
+                hop_dict[hop_type] = {"Hop Type": hop_type, "Attributes": []}
+
+            if attribute not in hop_dict[hop_type]["Attributes"]:
+                hop_dict[hop_type]["Attributes"].append(attribute)
+
+        hop_list = list(hop_dict.values())
+
+
+        return hop_list
